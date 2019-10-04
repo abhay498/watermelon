1f64247f4e3166ba401967e0b4716ff4859681a9
Abhay Kumar Singh <abskumar798@gmail.com>
2019-10-04 05:55:34 +0530

    Update wm_python.py

--- wm_python.py
+++ wm_python.py
@@ -2132,3 +2132,23 @@ for num in islice(count(), 5, 50, 10):
 
 print('I started at 5 and leaped by 10 till 50.')
 #--------------------------------------------------------------------------------------
+72. Shallow copy and Deep copy
+
+
+import copy
+color1 = ['Red', 'Blue']
+color2 = ['White','Black']
+color3 = [color1 , color2]
+# normal copy
+color4 = color3
+print (id(color3) == id(color4))        # True - color3 is the same object as color4
+print (id(color3[0]) == id(color4[0]))  # True - color4[0] is the same object as color3[0]
+# shallow copy
+color4 = copy.copy(color3)
+print (id(color3) == id(color4))        # False - color4 is now a new object
+print (id(color3[0]) == id(color4[0]))  # True - The new variable refers to the original variable.
+# deep  copy
+color4 = copy.deepcopy(color3)
+print (id(color3) == id(color4))        # False - color4 is now a new object
+print (id(color3[0]) == id(color4[0]))  # False - color4[0] is now a new object
+#--------------------------------------------------------------------------------------
