ee97ff1f69938bcb3d2892ae9ccc734369c53436
Abhay Kumar Singh <abskumar798@gmail.com>
2019-10-28 19:31:02 +0530

    Update wm_python.py

--- wm_python.py
+++ wm_python.py
@@ -2447,3 +2447,102 @@ print(minimum)
 nums = {n**2 for n in range(10)}
 print(nums)
 #--------------------------------------------------------------------------------------
+83. # Selection sort
+
+A = [64, 25, 12, 22, 11] 
+
+for i in range(len(A)): 
+
+	min_id = i 
+	for j in range(i+1, len(A)): 
+		if A[min_id] > A[j]: 
+			min_id = j 	 
+	A[i], A[min_id] = A[min_id], A[i] 
+
+print ("After selection sort") 
+print (A)
+#--------------------------------------------------------------------------------------
+84. # Insertion sort
+
+def insertionSort(arr): 
+ 
+    for i in range(1, len(arr)): 
+  
+        key = arr[i] 
+
+        j = i-1
+        while j >=0 and key < arr[j] : 
+                arr[j+1] = arr[j] 
+                j -= 1
+        arr[j+1] = key 
+
+arr = [12, 11, 13, 5, 6] 
+insertionSort(arr) 
+print ("Sorted list after insertion sort is:") 
+print(arr)
+
+#--------------------------------------------------------------------------------------
+85. # Quick sort
+
+def partition(arr,low,high): 
+    i = ( low-1 )   
+    pivot = arr[high]
+  
+    for j in range(low , high): 
+ 
+        if   arr[j] <= pivot: 
+
+            i = i+1 
+            arr[i],arr[j] = arr[j],arr[i] 
+  
+    arr[i+1],arr[high] = arr[high],arr[i+1] 
+    return ( i+1 ) 
+
+def quickSort(arr,low,high): 
+    if low < high: 
+        pi = partition(arr,low,high)  
+        quickSort(arr, low, pi-1) 
+        quickSort(arr, pi+1, high) 
+ 
+arr = [10, 7, 8, 9, 1, 5] 
+n = len(arr) 
+quickSort(arr,0,n-1) 
+print ("Sorted array after quick sort is:") 
+print(arr)
+
+#--------------------------------------------------------------------------------------
+86. # Heap sort
+
+def heapify(arr, n, i): 
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and arr[i] < arr[l]: 
+        largest = l 
+  
+    if r < n and arr[largest] < arr[r]: 
+        largest = r 
+
+    if largest != i: 
+        arr[i],arr[largest] = arr[largest],arr[i]
+  
+        heapify(arr, n, largest) 
+  
+def heapSort(arr): 
+    n = len(arr) 
+  
+    for i in range(n, -1, -1): 
+        heapify(arr, n, i) 
+  
+    for i in range(n-1, 0, -1): 
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0) 
+  
+arr = [ 12, 11, 13, 5, 6, 7] 
+heapSort(arr) 
+n = len(arr) 
+print ("Sorted array is") 
+print(arr)
+
+#--------------------------------------------------------------------------------------
