e474bd688e9c052826a2270e35fea5a1f39fe8f3
Abhay Kumar Singh <abskumar798@gmail.com>
2022-08-30 21:43:37 +0530

    Merge sort and quick sort in another repository

--- wm_python.py
+++ wm_python.py
@@ -1572,48 +1572,9 @@ link.postorder()
 print()
 
 #--------------------------------------------------------------------------------------
-50. """ Merge sort"""
+# 50. 
 
-def merge_sort(a_list):
-    print('Splitting', a_list)
 
-    if len(a_list) >  1:
-        mid = len(a_list) // 2
-
-        left_half = a_list[:mid]
-        right_half = a_list[mid:]
-
-        merge_sort(left_half)
-        merge_sort(right_half)
-
-        i = 0
-        j = 0
-        k = 0
-
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i] < right_half[j]:
-                a_list[k] = left_half[i]
-                i += 1
-            else:
-                a_list[k] = right_half[j]
-                j += 1
-
-            k += 1
-
-        while i < len(left_half):
-            a_list[k] = left_half[i]
-            i += 1
-            k += 1
-
-        while j < len(right_half):
-            a_list[k] = right_half[j]
-            j += 1
-            k += 1
-    print('Merging ',a_list)
-
-a_list = [int(x) for x in input().split()]
-merge_sort(a_list)
-print(a_list)
 
 #--------------------------------------------------------------------------------------
 51. """ Bubble sort """
@@ -2729,37 +2690,7 @@ print('List after insertion sort : ', arr)
 #>>> 
 
 #--------------------------------------------------------------------------------------
-85. # Quick sort
-
-#Source : https://www.geeksforgeeks.org/python-program-for-quicksort/
-
-def partition(arr, low, high):
-    i = low - 1
-    pivot = arr[high]
-
-    for j in range(low, high):
-        if arr[j] <= pivot:
-
-            i += 1
-            arr[i], arr[j] = arr[j], arr[i]
-
-    arr[i + 1], arr[high] = arr[high], arr[i + 1]
-    return i + 1
-
-
-def quick_sort(arr, low, high):
-    if len(arr) == 1:
-    	return arr
-    if low < high:
-        pi = partition(arr, low, high)
-        quick_sort(arr, low, pi - 1)
-        quick_sort(arr, pi + 1, high)
-
-
-arr = [10, 7, 8, 9, 1, 5]
-n = len(arr)
-quick_sort(arr, 0, n - 1)
-print("List after quick sort : ", arr)
+# 85.
 
 #--------------------------------------------------------------------------------------
 # 86. 
