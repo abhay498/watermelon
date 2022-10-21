1cf19824be6865177286b05a783e36217f075a08
Abhay Kumar Singh <abskumar798@gmail.com>
2022-10-21 06:29:50 +0530

    Shifted knuth morris pratt algorithm to gitlab

--- wm_python.py
+++ wm_python.py
@@ -3489,50 +3489,7 @@ print(array)
 # >>> 
 
 #--------------------------------------------------------------------------------------
-110. # Knuth Morris Pratt algorithm
-     # Source : https://www.algoexpert.io/questions/Knuth%E2%80%94Morris%E2%80%94Pratt%20Algorithm
-
-def knuthMorrisPrattAlgorithm(string, substring):
-    pattern = buildPattern(substring)
-    return doesMatch(string, substring, pattern)
-
-def buildPattern(substring):
-    pattern = [-1 for i in substring]
-    j = 0
-    i = 1
-    while i < len(substring):
-        if substring[i] == substring[j]:
-            pattern[i] = j
-            i += 1
-            j += 1
-        elif j > 0:
-            j = pattern[j - 1] + 1
-        else:
-            i += 1
-    return pattern
-
-def doesMatch(string, substring, pattern):
-    i = 0
-    j = 0
-    while i +len(substring) - j <= len(string):
-        if string[i] == substring[j]:
-            if j == len(substring) - 1:
-                return True
-            i += 1
-            j += 1
-        elif j > 0:
-            j = pattern[j - 1] + 1
-        else:
-            i += 1
-    return False
-            
-print(knuthMorrisPrattAlgorithm("aefoaefcdaefcdaed", "aefcdaed"))
-
-# o/p:
-# >>> 
-# =============== RESTART: C:\Users\Abhay\OneDrive\Desktop\rough.py ==============
-# True
-# >>> 
+110.
 
 #--------------------------------------------------------------------------------------
 111. # Group of stars 
