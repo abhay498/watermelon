0a05750ac62ca3fa797a68de58d19870d194d536
Abhay Kumar Singh <abskumar798@gmail.com>
2023-01-31 16:56:52 +0530

    Update wm_python.py

--- wm_python.py
+++ wm_python.py
@@ -255,48 +255,7 @@ else:
     print('{0} is not present'.format(num))
 
 #--------------------------------------------------------------------------------------
-#10. Binary search
-# Source : https://www.geeksforgeeks.org/python-program-for-binary-search/
-	
-# Iterative Binary Search Function
-# It returns index of x in given array arr if present,
-# else returns -1
-def binary_search(arr, x):
-	low = 0
-	high = len(arr) - 1
-	mid = 0
-
-	while low <= high:
-
-		mid = (high + low) // 2
-
-		if arr[mid] < x:
-			low = mid + 1
-
-		elif arr[mid] > x:
-			high = mid - 1
-
-		else:
-			return mid
-
-	return -1
-
-arr = [2, 3, 4, 10, 40]
-x = int(input('Enter the number to search '))
-
-result = binary_search(arr, x)
-
-if result != -1:
-	print('Number is present at index {0}'.format(result))
-else:
-	print('Number is not present in the list')
-
-# o/p
-# >>> 
-# =============== RESTART: C:\Users\Abhay\OneDrive\Desktop\rough.py ==============
-# Enter the number to search 2
-# Number is present at index 0
-# >>> 
+#10.
 
 #--------------------------------------------------------------------------------------
 #11. Decimal to binary
